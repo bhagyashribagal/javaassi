8026cd8377b4d1ad3136c0bcf75dd418cdcc9c4d
bhagyashribagal <97240754+bhagyashribagal@users.noreply.github.com>
2022-01-14 21:09:44 +0530

    Add files via upload
    1.Convert String into primitive data type

---
+++ Typecasting.java
@@ -0,0 +1,43 @@
+
+public class Typecasting {
+public static void main(String[] args) {
+		
+		//implicit conversion
+		System.out.println("Implicit Type Casting");
+		char a='A';
+		System.out.println("Value of a: "+a);
+		
+		int b=a;
+		System.out.println("Value of b: "+b);
+		
+		float c=a;
+		System.out.println("Value of c: "+c);
+		
+		long d=a;
+		System.out.println("Value of d: "+d);
+		
+		double e=a;
+		System.out.println("Value of e: "+e);
+		
+				
+		System.out.println("\n");
+		
+		System.out.println("Explicit Type Casting");
+		//explicit conversion
+		
+		double x=45.5;
+		int y=(int)x;
+		float z=(float)x;
+		short s = (short)x;
+		long l = (long)x;
+		//boolean bo =(boolean)x; CTE
+		System.out.println("Value of x: "+x);
+		System.out.println("Value of y: "+y);
+		System.out.println("Value of z: "+z);
+		System.out.println("Value of s: "+s);
+		System.out.println("Value of l: "+l);
+		//System.out.println("Value of bo: "+bo);
+	}
+
+
+}
